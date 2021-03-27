e6fd1bc4864c3ba0754af459f9944bfeeb0a9c57
EunJaePark <design0728@naver.com>
2021-03-27 15:39:02 +0900

    feat: login 페이지 레이아웃 구현(PC size)

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import IndexPage from 'pages'
 import categoryPage from './pages/category'
 import alarmDrawerPage from './pages/alarmDrawer'
+import loginPage from './pages/login'
 import React from 'react'
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import DefaultLayout from 'components/layout/defaultLayout'
@@ -14,9 +15,10 @@ function App() {
       <DefaultLayout>
         <Router basename="/iguana">
           <Switch>
+            <Route exact path="/" component={IndexPage} />
             <Route path="/category" component={categoryPage} />
             <Route path="/alarmdrawer" component={alarmDrawerPage} />
-            <Route exact path="/" component={IndexPage} />
+            <Route path="/login" component={loginPage} />
           </Switch>
         </Router>
       </DefaultLayout>
--- src/components/atom/RrIcon.tsx
+++ src/components/atom/RrIcon.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const RrIcon = () => {
-  return <div></div>
+const RrIcon = ({src, alt, size, top, left}) => {
+  return <Img src={src} alt={alt} size={size} top={top} left={left} />
 }
 
 export default RrIcon
+
+const Img = styled.img<{size: number, top: number, left: number}>`
+  width: ${ props => props.size }px;
+  height: ${ props => props.size }px;
+
+  position: absolute;
+  top: ${ props => props.top }px;
+  left: ${ props => props.left }px;
+`
